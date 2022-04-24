f902eaee52ac84891fd61ef97a5d1e95d383d83a
Sapir Ben David <79972748+SapirBenda@users.noreply.github.com>
2022-04-24 11:11:08 +0300

    build and run commands for example

--- auto_run.sh
+++ auto_run.sh
@@ -1,16 +1,12 @@
 # build and run 
 python3 setup.py build_ext --inplace
 
-# python3 spkmeans.py 0 wam input_1.txt
-# python3 spkmeans.py 0 ddg input_1.txt
-# python3 spkmeans.py 2 lnorm input_1.txt
-# python3 spkmeans.py 2 jacobi input_1.txt
-# python3 spkmeans.py 2 spk input_1.txt
+python3 spkmeans.py 0 wam inputs_examples/data_points/input_0.txt
+python3 spkmeans.py 0 ddg inputs_examples/data_points/input_1.txt
+python3 spkmeans.py 0 lnorm inputs_examples/data_points/input_2.txt
+python3 spkmeans.py 0 spk inputs_examples/data_points/input_3.txt
+python3 spkmeans.py 0 jacobi inputs_examples/sym_matrix/sym_matrix_input_4.txt
 
-python3 spkmeans.py 0 wam test.csv
-python3 spkmeans.py 0 ddg test.csv
-python3 spkmeans.py 0 lnorm test.csv
-python3 spkmeans.py 0 jacobi Lnorm.txt
-python3 spkmeans.py 0 spk test.csv
+
 
 
